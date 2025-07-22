e67fdc19b0b714a2fc3ae1a37987afb456e6fc64
Susll1k <sultanydyrys.2.0@gmail.com>
2025-07-22 12:26:07 +0500

    adminpanel

--- WEB/back-end/app/admin.py
+++ WEB/back-end/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Group, Subject, Lecture
+from .models import User, Group, Subject, Lecture, Schedule
 
 admin.site.register(User)
 admin.site.register(Group)
 admin.site.register(Subject)
 admin.site.register(Lecture)
+admin.site.register(Schedule)
